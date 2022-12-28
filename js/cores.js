bd1378e95ca8c500603fde4ba4c313d760b493e3
unknown <katispero@gmail.com>
2022-12-28 13:29:02 -0300

    Adicionando o projeto midi Alura

---
+++ js/cores.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+const quadroTintas = document.querySelector(".lista")
+const btnListarTintasQuadro = document.querySelector("#ListarTintasQuadro")
+const btnApagarTintasQuadro = document.querySelector("#ApagarTintasQuadro")
+
+
+	
+btnListarTintasQuadro.addEventListener("click", () => {
+		   console.log("funcionou")
+	})
+
